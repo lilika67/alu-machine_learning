45670648701a8bfc5502aeb07bf0a40ca1a586b1
lilika67 <l.kayitesi@alustudent.com>
2024-09-19 11:43:04 -0700

    linear algebra

--- math/advanced_linear_algebra/5-definiteness.py
+++ math/advanced_linear_algebra/5-definiteness.py
@@ -1,48 +1,50 @@
 #!/usr/bin/env python3
 """
-Checks for the definiteness of a matrix and returns string
+Defines function that calculates the definiteness of a matrix
 """
+
+
 import numpy as np
 
 
 def definiteness(matrix):
     """
-    returns a string about the definiteness
+    Calculates the definiteness of a matrix
+
+    parameters:
+        matrix [numpy.ndarray of shape(n, n)]:
+            matrix whose definiteness should be calculated
+
+    returns:
+        one of the following strings indicating definiteness or None:
+            "Positive definite"
+            "Positive semi-definite"
+            "Negative definite"
+            "Negative semi-definite"
+            "Indefinite"
     """
-    if not isinstance(matrix, np.ndarray):
+    if type(matrix) is not np.ndarray:
         raise TypeError("matrix must be a numpy.ndarray")
-
-    # Check if matrix is square
-    if matrix.ndim != 2 or matrix.shape[0] != matrix.shape[1]:
+    if len(matrix.shape) != 2 or matrix.shape[0] != matrix.shape[1] or \
+       np.array_equal(matrix, matrix.T) is False:
         return None
-
-    # Check if matrix is symmetric
-    if not np.allclose(matrix, matrix.T):
-        return None
-
-    try:
-        # Calculate eigenvalues
-        eigenvalues = np.linalg.eigvals(matrix)
-    except np.linalg.LinAlgError:
-        # If eigenvalue calculation fails, the matrix is not valid
-        return None
-
-    # Check for complex eigenvalues
-    if np.iscomplexobj(eigenvalues):
-        return None
-
-    # Determine definiteness based on eigenvalues
-    positive = np.all(eigenvalues > 0)
-    negative = np.all(eigenvalues < 0)
-    zero = np.any(np.isclose(eigenvalues, 0))
-
-    if positive and not zero:
-        return "Positive definite"
-    elif np.all(eigenvalues >= 0):
-        return "Positive semi-definite"
-    elif negative and not zero:
-        return "Negative definite"
-    elif np.all(eigenvalues <= 0):
-        return "Negative semi-definite"
-    else:
-        return "Indefinite"
+    pos_count = 0
+    neg_count = 0
+    zero_count = 0
+    eigenvalues = np.linalg.eig(matrix)[0]
+    for value in eigenvalues:
+        if value > 0:
+            pos_count += 1
+        if value < 0:
+            neg_count += 1
+        if value == 0 or value == 0.:
+            zero_count += 1
+    if pos_count and zero_count and neg_count == 0:
+        return ("Positive semi-definite")
+    elif neg_count and zero_count and pos_count == 0:
+        return ("Negative semi-definite")
+    elif pos_count and neg_count == 0:
+        return ("Positive definite")
+    elif neg_count and pos_count == 0:
+        return ("Negative definite")
+    return ("Indefinite")
