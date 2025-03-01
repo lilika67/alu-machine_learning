c9763775a873fce0d612c7545d7f9c182162b7a0
lilika67 <l.kayitesi@alustudent.com>
2025-03-01 21:54:39 +0200

    apis

--- pipeline/apis/4-rocket_frequency.py
+++ pipeline/apis/4-rocket_frequency.py
@@ -1,30 +1,50 @@
 #!/usr/bin/env python3
-"""Pipeline Api"""
+"""Script that displays the number of launches per rocket"""
+
+
 import requests
+from collections import defaultdict
+
+
+def get_launches_per_rocket():
+    """Script that displays the number of launches per rocket"""
+
+    launches_url = 'https://api.spacexdata.com/v4/launches'
+    rockets_url = 'https://api.spacexdata.com/v4/rockets'
+
+    try:
+        launches_response = requests.get(launches_url)
+        launches_response.raise_for_status()
+        launches = launches_response.json()
+
+        launch_count = defaultdict(int)
+        for launch in launches:
+            rocket_id = launch['rocket']
+            launch_count[rocket_id] += 1
+
+        rockets_response = requests.get(rockets_url)
+        rockets_response.raise_for_status()
+        rockets = rockets_response.json()
+
+        rocket_names = {rocket['id']: rocket['name'] for rocket in rockets}
+
+        rocket_launches = [
+            (rocket_names[rocket_id], count)
+            for rocket_id, count in launch_count.items()
+            ]
+
+        rocket_launches.sort(key=lambda x: (-x[1], x[0]))
+
+        for rocket, count in rocket_launches:
+            print("{}: {}".format(rocket, count))
+
+    except requests.RequestException as e:
+        print(
+            'An error occurred while making an API request: {}'.format(e))
+    except Exception as err:
+        print('A general error occurred: {}'.format(err))
+
 
 if __name__ == '__main__':
-    """pipeline api"""
-    url = "https://api.spacexdata.com/v4/launches"
-    r = requests.get(url)
-
-    if r.status_code != 200:
-        print("Failed to fetch launches")
-        exit(1)
-
-    rocket_dict = {}
-
-    for launch in r.json():
-        rocket_id = launch.get("rocket")
-        if rocket_id:
-            rocket_dict[rocket_id] = rocket_dict.get(rocket_id, 0) + 1
-
-    for key, value in sorted(rocket_dict.items(), key=lambda kv: kv[1], reverse=True):
-        rurl = f"https://api.spacexdata.com/v4/rockets/{key}"
-        req = requests.get(rurl)
-
-        if req.status_code == 200:
-            rocket_data = req.json()
-            rocket_name = rocket_data.get("name", "Unknown Rocket")
-            print(f"{rocket_name}: {value}")
-        else:
-            print(f"Failed to fetch rocket details for ID: {key}")
+    get_launches_per_rocket()
+    
